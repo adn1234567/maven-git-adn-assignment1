b015658ffcdbab551d7a84933c8a6f113e351c22
adn1234567 <18061919@myrp.edu.sg>
2023-07-14 13:24:55 -0700

    Intital commit push to master

---
+++ src/main/java/com/sddevops/maven_adn_assign1/eclipse/App.java
@@ -0,0 +1,13 @@
+package com.sddevops.maven_adn_assign1.eclipse;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
